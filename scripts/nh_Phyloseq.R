8a0193be03e93ee654f09fe98b6e1e5c478d52ea
millermorganOSU <millemo2@oregonstate.edu>
2023-08-04 17:06:02 -0700

    pptx needs polish

--- scripts/nh_Phyloseq.R
+++ scripts/nh_Phyloseq.R
@@ -316,7 +316,7 @@ ggsave("outputs/phyloseq_figures/ORD_B_NMDS_ASV.png", width = 13.333, height = 7
 # Plot ordination NMDS (by Disease)
 SAM.ordination.nmds.dis.b <- plot_ordination(ps.clean, ps.ord.nmds.b, type="sample", color="Disease", title="Sample Ordination (NMDS, Bray-Curtis)") + 
   geom_point(size=2) +
-  geom_point(shape = 1,size = 2,colour = "black")
+  geom_point(shape = 1,size = 2,colour = "black") +
 SAM.ordination.nmds.dis.b
 ggsave("outputs/phyloseq_figures/ORD_B_NMDS_SAM.png", width = 13.333, height = 7.5)
 
@@ -428,52 +428,18 @@ patchPCA
 
 ggsave("outputs/phyloseq_figures/ORD_PCA_PATCH.png", width = 13.333, height = 7.5)
 
-#### Investigating NMDS ordination to see if we actually have low variability
-#### Turns out it was just an outlier, a sample with virtually no ASVs
 
-### using phyloseq's distance() -> vegan metaMDS
-dist_1 <- as.matrix(distance(ps.clean, method = "bray", type = "samples"))
-NMDS1 <- metaMDS(dist_1, k = 2, trymax = 100, trace = F)
-stressplot(NMDS1)
-jpeg(filename="outputs/phyloseq_figures/Stress_plot_phyloseq.png", width = 7, height = 4, units = 'in', res = 300)
-stressplot(NMDS1)
-dev.off()
-
-dist_1[1:5,1:5]
-plot(NMDS1, type = "t")
-hist(dist_1)
-
-### using vegan
-dist_2 <-as.matrix(vegdist(clean.seq.tab, method = "bray"))
-NMDS2 <- metaMDS(dist_2, k = 2, trymax = 100, trace = F)
-stressplot(NMDS2)
-jpeg(filename="outputs/phyloseq_figures/Stress_plot_vegan.png", width = 7, height = 4, units = 'in', res = 300)
-stressplot(NMDS2)
-dev.off()
-
-dist_2[1:5,1:5]
-plot(NMDS2, type = "t")
-hist(dist_2)
-
-### is number of dimensions an issue? Even with one dimension, stress = zero
-### after removal of outlier, stress plot behaving as expected although a little high
-NMDS.scree <- function(x) { #where x is the name of the data frame variable
-  plot(rep(1, 10), replicate(10, metaMDS(x, autotransform = F, k = 1)$stress), xlim = c(1, 10),ylim = c(0, 0.30), xlab = "# of Dimensions", ylab = "Stress", main = "NMDS stress plot")
-  for (i in 1:10) {
-    points(rep(i + 1,10),replicate(10, metaMDS(x, autotransform = F, k = i + 1)$stress))
-  }
-}
-#NMDS.scree(dist_1)
-#NMDS.scree(dist_2)
-
-# RDA still clumped?
-rda1 <- rda(otu_table(ps.clean))
-rda2 <- rda(clean.seq.tab)
-rda
-plot(rda1, display = "sites")
-plot(rda2)
-text(rda2, labels=colSums(clean.seq.tab))
-text(rda1, labels=(tax_table(ps.clean))[,7], cex = 1)
+# Plot ordination NMDS with rings
+SAM.ordination.nmds.dis.b.e <- plot_ordination(ps.clean, ps.ord.nmds.b, type="sample", color="Disease", title="Sample Ordination (NMDS, Bray-Curtis)") + 
+  geom_point(size=4) +
+  geom_point(shape = 1,size = 4,colour = "black") +
+  stat_ellipse(type = "t", linewidth = 2) +
+  theme_bw() +
+  labs(caption = "Ellipses created using multivariate 't' distribution\nFinal Stress: 0.205") +
+  theme(plot.caption.position = "plot",
+        plot.caption = element_text(hjust = 1))
+  SAM.ordination.nmds.dis.b.e
+ggsave("outputs/phyloseq_figures/ORD_B_NMDS_SAM_ELLIPSES.png", width = 13.333, height = 7.5)
 
 # Relative abundance by disease - not amazing
 ps.t <- transform_sample_counts(ps.clean, function(x) x / sum(x) * 100)
@@ -690,7 +656,7 @@ my.comparison <- list( c("Canker", "Healthy"))
 symnum.args = list(cutpoints = c(0, 0.0001, 0.001, 0.01, 0.05, 1), symbols = c("****", "***", "**", "*", "ns"))
 
 
-plot_richness(ps.clean,
+Alpha.plot <- plot_richness(ps.clean,
               x="Disease",
               measures=c("Shannon", "Simpson", "InvSimpson", "Observed"),
               color="Disease") +
@@ -707,7 +673,7 @@ ggsave("outputs/phyloseq_figures/alpha_diversity_boxplot_observed.png", units = 
 
 # Beta diversity
 
-# Premanova test using the vegan package
+# Permanova test using the vegan package
 metadata <- as(sample_data(ps.clean), "data.frame")
 
 bray <- distance(ps.clean, method="bray")
@@ -716,11 +682,24 @@ jaccard <- distance(ps.clean, method="jaccard")
 pre.bray <- adonis2(bray ~ Disease, data = metadata)
 pre.jaccard <- adonis2(jaccard ~ Disease, data = metadata)
 
+pre.bray
+pre.jaccard
+
 beta.bray <- betadisper(bray, metadata$Disease)
-beta.jaccard <- betadisper(bray, metadata$Disease)
+beta.jaccard <- betadisper(jaccard, metadata$Disease)
 permutest(beta.bray)
 permutest(beta.jaccard)
 
+plot(beta.bray, identify.or)
+plot(beta.jaccard)
+boxplot(beta.bray)
+boxplot(beta.jaccard)
+eigenvals(beta.bray)
+eigenvals(beta.jaccard)
+TukeyHSD(beta.bray, which = "group", ordered = FALSE,
+         conf.level = 0.95)
+TukeyHSD(beta.jaccard, which = "group", ordered = FALSE,
+         conf.level = 0.95)
 # Misc.
 
 unique(fungal.taxa.clean[,2])
@@ -752,6 +731,33 @@ dim(seqtab.nochim)
 dis.class.dis <- as.matrix(table(wood.meta.clean$Disease))
 gen.dis <- as.matrix(table(wood.meta.clean$Genotype))
 which(gen.dis[,1]==2)
+
+canker.library <- as.matrix(ps.clean %>%
+                              ps_filter(Disease == "Canker") %>%
+                              sample_sums())
+
+mean(canker.library[,1])
+
+healthy.library <- as.matrix(ps.clean %>%
+                              ps_filter(Disease == "Healthy") %>%
+                              sample_sums())
+
+mean(healthy.library[,1])
+
+non.library <- as.matrix(ps.clean %>%
+                               ps_filter(Disease == "Non-canker") %>%
+                               sample_sums())
+
+mean(non.library[,1])
+
+dist_ex <- dist_1[1:10,1:10]
+
+write.csv(dist_ex, "outputs/distance_example.csv")
+
+richness <- estimate_richness(ps.clean, measures="Shannon")
+richness_ex <- head(richness, 10)
+
+write.csv(richness_ex, "outputs/richness_example.csv")
 
 # ### Add total occurrence
 # # The code below was a test that wasn't very useful
@@ -879,3 +885,54 @@ which(gen.dis[,1]==2)
 #   aes(reorder(Species, -Abundance, sum))
 # ASV.per.sp
 # ggsave("outputs/phyloseq_figures/ASV_Abundance_per_Sample_sorted.png", width = 9, height = 6)
+
+#### Investigating NMDS ordination to see if we actually have low variability
+#### Turns out it was just an outlier, a sample with virtually no ASVs
+
+# ### using phyloseq's distance() -> vegan metaMDS
+#dist_1 <- as.matrix(distance(ps.clean, method = "bray", type = "samples"))
+#NMDS1 <- metaMDS(dist_1, k = 2, trymax = 100, trace = F)
+#NMDS1
+#stressplot(NMDS1, main = "NMDS Sample Ordination Stress Plot")
+#mtext(stp, "Final Stress: 0.205")
+#NMDS1$stress
+# jpeg(filename="outputs/phyloseq_figures/Stress_plot_phyloseq.png", width = 7, height = 4, units = 'in', res = 300)
+# stressplot(NMDS1)
+# dev.off()
+# 
+# dist_1[1:5,1:5]
+# plot(NMDS1, type = "t")
+# hist(dist_1)
+# 
+# ### using vegan
+#dist_2 <-as.matrix(vegdist(clean.seq.tab, method = "jaccard"))
+#NMDS2 <- metaMDS(dist_2, k = 2, trymax = 100, trace = F)
+#stressplot(NMDS2)
+# jpeg(filename="outputs/phyloseq_figures/Stress_plot_vegan.png", width = 7, height = 4, units = 'in', res = 300)
+# stressplot(NMDS2)
+# dev.off()
+# 
+# dist_2[1:5,1:5]
+# plot(NMDS2, type = "t")
+# hist(dist_2)
+# 
+# ### is number of dimensions an issue? Even with one dimension, stress = zero
+# ### after removal of outlier, stress plot behaving as expected although a little high
+# NMDS.scree <- function(x) { #where x is the name of the data frame variable
+#   plot(rep(1, 10), replicate(10, metaMDS(x, autotransform = F, k = 1)$stress), xlim = c(1, 10),ylim = c(0, 0.30), xlab = "# of Dimensions", ylab = "Stress", main = "NMDS stress plot")
+#   for (i in 1:10) {
+#     points(rep(i + 1,10),replicate(10, metaMDS(x, autotransform = F, k = i + 1)$stress))
+#   }
+# }
+# #NMDS.scree(dist_1)
+# #NMDS.scree(dist_2)
+
+# # RDA still clumped?
+# rda1 <- rda(otu_table(ps.clean))
+# rda2 <- rda(clean.seq.tab)
+# rda
+# plot(rda1, display = "sites")
+# plot(rda2)
+# text(rda2, labels=colSums(clean.seq.tab))
+# text(rda1, labels=(tax_table(ps.clean))[,7], cex = 1)
+
